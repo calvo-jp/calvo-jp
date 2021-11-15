668d009e0fe7758ab6b068b1170f17445e60df27
jp calvo <calvojp92@gmail.com>
2021-11-15 11:09:42 +0800

    Setup tailwindcss

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import * as React from 'react';
+
+const Landing = () => {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>JP Calvo</title>
+      </Head>
+
+      <h1 className="text-2xl">Hello World</h1>
+    </React.Fragment>
+  );
+};
+
+export default Landing;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './src/pages/**/*.tsx',
+    './src/widgets/**/*.tsx',
+    './src/layouts/**/*.tsx',
+  ],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
