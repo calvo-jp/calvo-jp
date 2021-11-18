0f0cc026b7af6e94ef73f1eeedc2e99f514a0732
jp calvo <calvojp92@gmail.com>
2021-11-18 20:08:10 +0800

    Made responsive and emptied image temporarily.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Footer from 'layouts/Footer';
 import Header from 'layouts/Header';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 import * as React from 'react';
 
 const Landing = () => {
@@ -14,36 +15,30 @@ const Landing = () => {
       <div className="flex flex-col min-h-screen">
         <Header />
 
-        <main className="flex-grow flex p-2 items-center justify-center gap-14">
-          <section className="relative w-[300px] h-[300px] rounded-full overflow-hidden border-8 border-gray-100"></section>
+        <main className="flex-grow flex flex-col md:flex-row p-2 items-center justify-center gap-8 lg:gap-16">
+          <section className="w-[200px] h-[200px] md:w-[250px] md:h-[250px] lg:w-[300px] lg:h-[300px] relative rounded-full overflow-hidden border-8 border-gray-100"></section>
 
           <section className="flex flex-col items-center justify-center">
-            <About />
+            <p className="text-4xl md:text-5xl lg:text-6xl text-center">
+              <span>Hi, Iam</span>
+              <strong className="ml-6 font-bold text-blue-500">John</strong>
+              <span>.</span>
+            </p>
+
+            <p className="text-lg md:text-xl lg:text-2xl text-gray-600 mt-2 lg:mt-4 text-center">
+              Awesome developer of modern websites
+            </p>
+
+            <Link href="/chat" passHref>
+              <button className="mt-5 w-[150px] border-2 border-blue-500 p-3 rounded-2xl text-blue-500 font-bold text-lg hover:bg-blue-500 hover:text-white focus:bg-blue-500 focus:text-white focus:ring-4 focus:ring-blue-200 transition-colors duration-200 outline-none">
+                <span>Chat now</span>
+              </button>
+            </Link>
           </section>
         </main>
 
         <Footer />
       </div>
-    </React.Fragment>
-  );
-};
-
-const About = () => {
-  return (
-    <React.Fragment>
-      <p className="text-6xl">
-        <span>Hi, Iam</span>
-        <strong className="ml-6 text-6xl font-bold text-blue-500">John</strong>
-        <span>.</span>
-      </p>
-
-      <p className="text-2xl text-gray-600 mt-4">
-        Awesome developer of modern websites
-      </p>
-
-      <button className="mt-5 w-[150px] border-2 border-blue-500 p-3 rounded-2xl text-blue-500 font-bold text-lg hover:bg-blue-500 hover:text-white transition-colors duration-200">
-        <span>Chat now</span>
-      </button>
     </React.Fragment>
   );
 };
