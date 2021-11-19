dd2e016f4e5356effe3c754b0a0d6b65e7a45902
jp calvo <calvojp92@gmail.com>
2021-11-19 09:48:48 +0800

    Init projects page

---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import * as React from 'react';
+
+const Projects = () => {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>JP Calvo | Projects</title>
+      </Head>
+
+      <div className="min-h-screen flex flex-col"></div>
+    </React.Fragment>
+  );
+};
+
+export default Projects;
