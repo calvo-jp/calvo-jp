2b4021af4a740768092e67ba5b854836ca6b6265
jp calvo <calvojp92@gmail.com>
2021-11-20 06:52:42 +0800

    Refactored code

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -10,26 +10,17 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({ mobile, open }) => {
-  const desktop = !mobile;
-  const mobile_ = !!mobile;
-  const mobileShown = mobile_ && !!open;
-  const mobileHidden = mobile_ && !open;
-
   return (
     <nav
       className={clsx(
-        desktop && 'hidden md:flex',
-        mobileHidden && 'hidden',
-        mobileShown &&
+        !mobile && 'hidden md:flex',
+        mobile && !open && 'hidden',
+        mobile &&
+          open &&
           'flex md:hidden items-center justify-center fixed top-0 left-0 h-full w-full z-10 bg-black bg-opacity-90 text-gray-300 text-2xl'
       )}
     >
-      <ul
-        className={clsx(
-          mobile_ && 'flex flex-col gap-1 items-center',
-          desktop && 'flex gap-3'
-        )}
-      >
+      <ul className={clsx('flex gap-3', mobile && 'flex-col items-center')}>
         <li>
           <NavbarLink href="/">About</NavbarLink>
         </li>
@@ -53,26 +44,22 @@ const NavbarLink: React.FC<NavbarLinkProps> = ({ href, children }) => {
   const router = useRouter();
   const [state, dispatch] = useGlobalState();
 
-  const handleClick = () => {
-    if (state.navbarOpen) {
-      dispatch({
-        type: 'navbar.toggle',
-      });
-    }
+  const ensureMobileNavbarCloses = () => {
+    if (state.navbarOpen) dispatch({ type: 'navbar.toggle' });
   };
 
   return (
-    <div
-      className={clsx(
-        'py-2',
-        router.pathname !== href && 'md:hover:text-yellow-600',
-        router.pathname === href && 'md:text-blue-600 text-blue-400'
-      )}
-    >
-      <Link href={href} passHref>
-        <a onClick={handleClick}>{children}</a>
-      </Link>
-    </div>
+    <Link href={href} passHref>
+      <a
+        onClick={ensureMobileNavbarCloses}
+        className={clsx(
+          router.pathname !== href && 'md:hover:text-yellow-600',
+          router.pathname === href && 'md:text-blue-600 text-blue-400'
+        )}
+      >
+        {children}
+      </a>
+    </Link>
   );
 };
 
