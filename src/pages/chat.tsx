e8d4563a695e1d7c6379d00ebbab109b35fce4e0
jp calvo <calvojp92@gmail.com>
2021-11-19 09:51:05 +0800

    Init chat page

---
+++ src/pages/chat.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import * as React from 'react';
+
+const Chat = () => {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>JP Calvo | Chat with John</title>
+      </Head>
+
+      <div className="min-h-screen"></div>
+    </React.Fragment>
+  );
+};
+
+export default Chat;
