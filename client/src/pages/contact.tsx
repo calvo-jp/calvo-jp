1db103b8b2c8c9743c5a50d6b703265bd390c06b
jp calvo <calvojp92@gmail.com>
2021-11-23 18:51:16 +0800

    added redirect to gmail

--- client/src/pages/contact.tsx
+++ client/src/pages/contact.tsx
@@ -73,9 +73,9 @@ const Contact = () => {
                 <p>
                   <span>Send an email to </span>
                   <a
-                    rel="noreferrer"
+                    href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=calvojp92@gmail.com"
                     target="_blank"
-                    href="https://www.google.com/mail"
+                    rel="noreferrer"
                     className="text-gray-700 hover:text-blue-600 transition-colors duration-300"
                   >
                     calvojp92@gmail.com
