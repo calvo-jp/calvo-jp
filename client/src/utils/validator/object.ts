b9220d729a652ec66aa94bac539b796b3800ccca
jp calvo <calvojp92@gmail.com>
2021-11-28 17:27:38 +0800

    updated isObject and added subject type

--- client/src/utils/validator/object.ts
+++ client/src/utils/validator/object.ts
@@ -6,25 +6,25 @@ interface Schema {
   [key: string]: NumberValidator | StringValidator | BooleanValidator;
 }
 
+type Subject<T> = Partial<Record<keyof T, any>> & Record<string, any>;
+
 type Result<T extends Schema> = {
   errors: { [_ in keyof T]?: string };
   values: { [P in keyof T]?: ReturnType<T[P]['validate']> };
 };
 
 class ObjectValidator {
-  validate<T extends Schema>(subject: T, data: unknown) {
+  validate<T extends Schema>(schema: T, subject: Subject<T>) {
     const result: Result<T> = {
       errors: {},
       values: {},
     };
 
-    // codes...
-
     return result;
   }
 }
 
-const isObject = (o: any): o is {} =>
+const isObject = (o: any): o is Record<string, any> =>
   Object(o) === o && Object.prototype.toString.call(o) === '[object Object]';
 
 export default ObjectValidator;
