3b582da2c8ddecd5fb5808fbc08b554850ba95f5
jp calvo <calvojp92@gmail.com>
2021-12-03 20:08:58 +0800

    used toggle icon instead of text

--- client/src/layouts/Header.tsx
+++ client/src/layouts/Header.tsx
@@ -2,16 +2,26 @@ import clsx from 'clsx';
 import useGlobalState from 'hooks/store/useGlobalState';
 import Link from 'next/link';
 import * as React from 'react';
+import MenuIcon from 'widgets/icons/Menu';
 import Navbar from './Navbar';
 
-interface HeaderProps {}
+type HeaderProps = React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLElement>,
+  HTMLElement
+>;
 
-const Header: React.FC<HeaderProps> = () => {
+const Header: React.FC<HeaderProps> = ({ children, className, ...props }) => {
   const [state] = useGlobalState();
 
   return (
     <React.Fragment>
-      <header className="flex justify-between items-center py-4 px-6">
+      <header
+        className={clsx(
+          'flex justify-between items-center py-4 px-6 bg-white',
+          className
+        )}
+        {...props}
+      >
         <Logo />
         <Navbar />
         <Hamburger />
@@ -48,7 +58,12 @@ const Hamburger = () => {
       className={clsx('z-20 md:hidden', state.navbarOpen && 'text-gray-400')}
       onClick={toggleNavbar}
     >
-      Toggle Menu
+      <MenuIcon
+        className={clsx(
+          state.navbarOpen && 'text-gray-400',
+          !state.navbarOpen && 'text-gray-600'
+        )}
+      />
     </button>
   );
 };
