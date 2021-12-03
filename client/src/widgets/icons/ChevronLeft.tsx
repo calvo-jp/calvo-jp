ad6c82c1a96f3467205c1449b17ecc80616deb5a
jp calvo <calvojp92@gmail.com>
2021-12-03 20:26:17 +0800

    added icon

---
+++ client/src/widgets/icons/ChevronLeft.tsx
@@ -0,0 +1,27 @@
+import clsx from 'clsx';
+import * as React from 'react';
+
+const ChevronLeftIcon: React.FC<React.SVGProps<SVGSVGElement>> = ({
+  className,
+  ...props
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className={clsx('h-6 w-6', className)}
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M15 19l-7-7 7-7"
+      />
+    </svg>
+  );
+};
+
+export default ChevronLeftIcon;
