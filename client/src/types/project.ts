a022c8d8fb749766c2fc7373b2df74dea9e09c21
jp calvo <calvojp92@gmail.com>
2021-12-03 20:25:58 +0800

    added keywords prop specific to metatags

--- client/src/types/project.ts
+++ client/src/types/project.ts
@@ -27,6 +27,9 @@ interface IProject {
   description: string;
   banner: string;
   screenshots: string[];
+
+  /** specific to keyword meta tag */
+  keywords: string[];
 }
 
 export default IProject;
