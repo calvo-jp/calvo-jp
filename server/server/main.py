b9d02206b1c20e5fa18dad7b0de176fc58d87b1e
jp calvo <calvojp92@gmail.com>
2021-12-04 20:40:45 +0800

    added origin whitelist for prod mode

--- server/server/main.py
+++ server/server/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from .config import config
+
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins='*',
+    allow_origins='*' if config.debug else 'https://calvojp.vercel.app',
     allow_methods='*',
     allow_headers='*',
 )
