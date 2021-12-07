992a393060c59d4c566568c71d2fa95adff70f4d
jp calvo <calvojp92@gmail.com>
2021-12-07 19:45:37 +0800

    moved rapidapi helper to email router

--- server/server/utils.py
+++ server/server/utils.py
@@ -1,36 +1,3 @@
-from typing import Optional
-
-import httpx
-
-from .config import config
-
-
-def send_email_via_rapidapi(
-    *,
-    sender: str,
-    recipient: str,
-    subject: Optional[str] = None,
-    body: str,
-):
-    url = 'https://fapimail.p.rapidapi.com/email/send'
-
-    data = {
-        'sender': sender,
-        'recipient': recipient,
-        'subject': subject,
-        'message': body
-    }
-
-    headers = {
-        'x-rapidapi-host': 'fapimail.p.rapidapi.com',
-        'x-rapidapi-key': config.rapidapi_key,
-    }
-
-    response = httpx.post(url=url, json=data, headers=headers)
-    response.raise_for_status()
-    return response.json()
-
-
 def camelize(subject: str):
     words = subject.split("_")
     array = []
