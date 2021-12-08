399b28f631ebff10e70f58679403d793930fc975
jp calvo <calvojp92@gmail.com>
2021-12-08 12:13:55 +0800

    added footer and back button

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -1,5 +1,8 @@
+import Footer from 'layouts/Footer';
 import Head from 'next/head';
+import Link from 'next/link';
 import * as React from 'react';
+import ChevronLeftIcon from 'widgets/icons/ChevronLeft';
 
 const NotFound = () => {
   return (
@@ -8,8 +11,23 @@ const NotFound = () => {
         <title>Error 404 | Page not found</title>
       </Head>
 
-      <div className="min-h-screen flex items-center justify-center p-2">
-        <h1 className="text-gray-300 text-9xl font-bold">4O4</h1>
+      <div className="min-h-screen flex flex-col">
+        <header className="bg-white py-4 px-6 flex items-center justify-between">
+          <Link href="/" passHref>
+            <a className="flex items-center gap-1">
+              <ChevronLeftIcon className="h-[14px] w-[14px]" />
+              <span>Go Home</span>
+            </a>
+          </Link>
+        </header>
+
+        <main className="flex-grow flex items-center justify-center p-2">
+          <div>
+            <h1 className="text-gray-300 text-9xl font-bold">4O4</h1>
+          </div>
+        </main>
+
+        <Footer />
       </div>
     </React.Fragment>
   );
