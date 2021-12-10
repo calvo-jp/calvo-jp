5dbf8c28c40b110770712549b361d8ff3782836b
jp calvo <calvojp92@gmail.com>
2021-12-10 16:39:49 +0800

    modularized models. huge update for project model

---
+++ server/server/models/project.py
@@ -0,0 +1,127 @@
+import enum
+import os
+from typing import Literal, Optional, TypedDict
+
+from PIL import Image, UnidentifiedImageError
+from pydantic import BaseModel
+
+from ..config import config
+
+TechStack = Literal[
+    # frontend frameworks, ui-kit, etc.
+    'react',
+    'nextjs',
+    'flutter',
+    'tailwind',
+    'material ui'
+    # frontend languages, pre-processors, etc.
+    'html',
+    'css',
+    'sass',
+    'javascript',
+    'typescript',
+    'dart',
+    # backend frameworks
+    'fastapi',
+    'express',
+    'nestjs',
+    # backend languages
+    'python',
+    'nodejs',
+    # orms and odms
+    'sqlalchemy',
+    'sqlmodel',
+    'prisma',
+    # databases
+    'postgres',
+    'sqlite3',
+    'mongodb',
+]
+
+
+class Orientation(str, enum.Enum):
+    SQUARE = 'square'
+    PORTRAIT = 'portrait'
+    LANDSCAPE = 'landscape'
+
+
+class ImageInfo(TypedDict):
+    url: str
+    width: float
+    height: float
+    orientation: Orientation
+
+
+class Project:
+    def __init__(
+        self,
+        *,
+        slug: str,
+        name: str,
+        description: str,
+        tags: Optional[list[str]] = None,
+        techstacks: Optional[list[TechStack]] = None,
+        banner: str,
+        screenshots: Optional[list[str]] = None,
+        # pylint: disable=unused-argument
+        **kwarg,
+        # pylint: enable=unused-argument
+    ):
+        self.slug = slug
+        self.name = name
+        self.description = description
+        self.tags = tags or []
+        self.techstacks = techstacks or []
+
+        self.__banner = banner
+        self.__screenshots = screenshots or []
+
+    @property
+    def screenshots(self):
+        imageinfolist = [_imageinfo(sc) for sc in self.__screenshots]
+        return [i for i in imageinfolist if i is not None]
+
+    @property
+    def banner(self):
+        return _imageinfo(self.__banner)
+
+
+def _imageorient(w: float, h: float) -> Orientation:
+    if h > w + h / 4:
+        return Orientation.PORTRAIT
+    if w > h + w / 4:
+        return Orientation.LANDSCAPE
+    return Orientation.SQUARE
+
+
+def _imageinfo(filename: str):
+    try:
+        fullpath = os.path.join(config.assets_dir, "images", filename)
+
+        image = Image.open(fullpath)
+
+        info = ImageInfo(
+            url=config.server_base_url + "/streams/images/" + filename,
+            width=image.width,
+            height=image.height,
+            orientation=_imageorient(image.width, image.height)
+        )
+
+        image.close()
+
+        return info
+    except (FileNotFoundError, UnidentifiedImageError):
+        return None
+
+
+class ReadProject(BaseModel):
+    slug: str
+    name: str
+    description: str
+    tags: list[str]
+    banner: Optional[ImageInfo] = None
+    screenshots: list[ImageInfo]
+    techstacks: list[TechStack]
+
+    class Config:
+        orm_mode = True
