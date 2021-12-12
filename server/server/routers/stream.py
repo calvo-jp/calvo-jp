9047d3f6e60a1a1a894cd09846163ee452efb413
jp calvo <calvojp92@gmail.com>
2021-12-12 14:09:14 +0800

    refactor

--- server/server/routers/stream.py
+++ server/server/routers/stream.py
@@ -2,6 +2,7 @@ import enum
 import os
 
 from fastapi import APIRouter, HTTPException, status
+from fastapi.param_functions import Path
 from fastapi.responses import FileResponse
 
 from ..config import config
@@ -9,14 +10,18 @@ from ..config import config
 router = APIRouter(prefix='/streams', tags=['streams'])
 
 
-class Media(str, enum.Enum):
+class MediaType(str, enum.Enum):
     IMAGES = 'images'
     VIDEOS = 'videos'
 
 
 @router.get(path='/{media}/{filename}', response_class=FileResponse)
-async def view_media(*, media: Media,  filename: str):
-    fullpath = os.path.join(config.assets_dir, media, filename)
+async def view_media(
+    *,
+    type_: MediaType = Path(..., alias='type'),
+    filename: str
+):
+    fullpath = os.path.join(config.assets_dir, type_, filename)
 
     if not os.path.exists(fullpath):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
