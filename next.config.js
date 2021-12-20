0fa70559767153174bb1a922730e4c65bd64d2fb
jp calvo <calvojp92@gmail.com>
2021-12-20 16:59:15 +0800

    removed stupid rewrites not working in server

--- next.config.js
+++ next.config.js
@@ -1,33 +1,8 @@
-const debug = process.env.NODE_ENV === "development";
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   poweredByHeader: false,
   images: {
     domains: ["localhost"],
-  },
-  rewrites: async () => {
-    return [
-      {
-        source: "/api/:path*",
-        destination: (() => {
-          if (debug) {
-            return "http://localhost:3000/api/:path*";
-          } else {
-            return "https://calvo-jp.vercel.app/api/:path*";
-          }
-        })(),
-      },
-    ];
-  },
-  redirects: async () => {
-    return [
-      {
-        source: "/",
-        destination: "/about",
-        permanent: true,
-      },
-    ];
   },
 };
