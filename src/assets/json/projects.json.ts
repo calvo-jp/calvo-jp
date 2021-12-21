add48eeec8d03669c35da2c08e7aafcbbdc3fd7e
jp calvo <calvojp92@gmail.com>
2021-12-21 11:08:45 +0800

    beta testing

--- src/assets/json/projects.json.ts
+++ src/assets/json/projects.json.ts
@@ -1,5 +1,39 @@
 import IProject from "types/project";
 
-const items: IProject[] = [];
+const items: IProject[] = [
+  {
+    slug: "recipes",
+    name: "Recipes",
+    description: "Find or search amazing recipes",
+    keywords: [
+      "cooking",
+      "ingredients",
+      "instructions",
+      "recipes",
+      "delicious",
+      "tasty",
+      "chef",
+      "food",
+      "yummy",
+    ],
+    techstacks: [
+      "nextjs",
+      "tailwind",
+      "python",
+      "fastapi",
+      "sqlmodel",
+      "postgres",
+    ],
+    screenshots: [
+      { path: "/images/", orientation: "square" },
+      { path: "/images/", orientation: "vertical" },
+      { path: "/images/", orientation: "horizontal" },
+      { path: "/images/", orientation: "horizontal" },
+      { path: "/images/", orientation: "square" },
+      { path: "/images/", orientation: "vertical" },
+    ],
+    banner: "/images/",
+  },
+];
 
 export default items;
