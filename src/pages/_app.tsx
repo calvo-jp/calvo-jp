43944b5d4e34afb6705b3bd07225a1d4e4c981e9
jp calvo <calvojp92@gmail.com>
2021-12-21 16:38:10 +0800

    updated meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,32 +6,62 @@ import "tailwindcss/tailwind.css";
 import LightboxProvider from "widgets/lightbox/LightboxProvider";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const description = [
+    "Official website of John Paul Calvo (aka JP Calvo), a web dev based in PH.",
+    "I am 29 years old and I am currently a freshman student at Cedar College, Inc.",
+    "I develop websites with modern look and feel and always ensures maintainability.",
+    "I also like to play guitar during weekends and loves coffee with less sugar.",
+  ].join("");
+
+  const keywords = [
+    "jp calvo",
+    "portfolio",
+    "freelancer",
+    "web developer",
+    "modern",
+    "website",
+    "awesome",
+    "programmer",
+    "css",
+    "html",
+    "python",
+    "nodejs",
+    "postgres",
+    "mongodb",
+    "sqlite",
+    "fastapi",
+    "sqlmodel",
+    "sqlalchemy",
+    "react",
+    "nextjs",
+    "tailwind",
+    "prisma",
+    "fastify",
+    "expressjs",
+  ].join();
+
   return (
     <React.Fragment>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="JP Calvo's portfolio" />
-        <meta name="keywords" content="jp calvo, portfolio" />
-        <meta name="author" content="jp calvo" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content="jp calvo <calvojp92@gmail.com>" />
 
         <meta
           property="og:url"
-          content="calvo-jp.vercel.app"
+          content="https://calvo-jp.vercel.app"
           key="meta.og.url"
         />
-        <meta
-          property="og:title"
-          content="JP Calvo (John Paul Calvo)"
-          key="meta.og.title"
-        />
+        <meta property="og:title" content="JP Calvo" key="meta.og.title" />
         <meta
           property="og:description"
-          content="JP Calvo's portfolio"
+          content={description}
           key="meta.og.description"
         />
         <meta
           property="og:keywords"
-          content="jp calvo, portfolio"
+          content={keywords}
           key="meta.og.keywords"
         />
       </Head>
