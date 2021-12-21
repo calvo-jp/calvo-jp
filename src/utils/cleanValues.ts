4c401edb51bb6a39da900eb6e13f7a5d31491a78
jp calvo <calvojp92@gmail.com>
2021-12-21 14:44:24 +0800

    removed dependency

--- src/utils/cleanValues.ts
+++ src/utils/cleanValues.ts
@@ -1,23 +1,34 @@
-import isObject from "./isObject";
+type Dictionary = Record<string, any>;
+type Dirty = "" | null;
+
+const isDictionary = (subject: any): subject is Dictionary =>
+  Object(subject) === subject &&
+  Object.prototype.toString.call(subject) === "[object Object]";
+
+const isDirty = (subject: any): subject is Dirty => {
+  // null values
+  if (subject === null) return true;
+
+  // empty string or string that contains nothing but spaces
+  if (typeof subject === "string" && subject.trim() === "") return true;
+
+  return false;
+};
+
+const cleanObject = (subject: Dictionary) => {
+  const values: Dictionary = {};
+
+  for (const [key, value] of Object.entries(subject))
+    if (!isDirty(value)) values[key] = cleanValues(value);
+
+  return values;
+};
 
 /** deeply removes falsy values in object or array of objects */
 const cleanValues = (subject: any): any => {
   if (Array.isArray(subject)) return subject.map(cleanValues);
-  if (isObject(subject)) return cleanObject(subject);
+  if (isDictionary(subject)) return cleanObject(subject);
   return subject;
-};
-
-const isFalsy = (subject: any) =>
-  subject === null || (typeof subject === "string" && subject.trim() === "");
-
-const cleanObject = (subject: Record<string, any>) => {
-  const values: Record<string, any> = {};
-
-  for (const [key, value] of Object.entries(subject)) {
-    if (!isFalsy(value)) values[key] = cleanValues(value);
-  }
-
-  return values;
 };
 
 export default cleanValues;
