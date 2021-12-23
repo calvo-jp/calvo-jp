41bb696414be918d0249933335a35b0f00bef950
jp calvo <calvojp92@gmail.com>
2021-12-23 08:32:59 +0800

    sample images from pexels.com

--- src/assets/json/projects.json.ts
+++ src/assets/json/projects.json.ts
@@ -24,15 +24,37 @@ const items: IProject[] = [
       "sqlmodel",
       "postgres",
     ],
+    banner: "/images/showcase/banners/recipes.jpg",
     screenshots: [
-      { path: "/images/", orientation: "square" },
-      { path: "/images/", orientation: "vertical" },
-      { path: "/images/", orientation: "horizontal" },
-      { path: "/images/", orientation: "horizontal" },
-      { path: "/images/", orientation: "square" },
-      { path: "/images/", orientation: "vertical" },
+      {
+        path: "/images/showcase/screenshots/recipes/1.jpg",
+        orientation: "horizontal",
+      },
+      {
+        path: "/images/showcase/screenshots/recipes/2.jpg",
+        orientation: "vertical",
+      },
+      {
+        path: "/images/showcase/screenshots/recipes/3.jpg",
+        orientation: "horizontal",
+      },
+      {
+        path: "/images/showcase/screenshots/recipes/4.jpg",
+        orientation: "vertical",
+      },
+      {
+        path: "/images/showcase/screenshots/recipes/5.jpg",
+        orientation: "horizontal",
+      },
+      {
+        path: "/images/showcase/screenshots/recipes/6.jpg",
+        orientation: "square",
+      },
+      {
+        path: "/images/showcase/screenshots/recipes/7.jpg",
+        orientation: "horizontal",
+      },
     ],
-    banner: "/images/",
   },
 ];
 
