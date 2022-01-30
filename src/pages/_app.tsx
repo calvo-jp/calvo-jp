2fa6e67f82e88e583e8e519a361c1c5a7a13253e
jp calvo <calvojp92@gmail.com>
2022-01-30 17:56:59 +0800

    removed custom font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,3 @@
-import '@fontsource/m-plus-code-latin/100.css';
-import '@fontsource/m-plus-code-latin/200.css';
-import '@fontsource/m-plus-code-latin/300.css';
-import '@fontsource/m-plus-code-latin/400.css';
-import '@fontsource/m-plus-code-latin/500.css';
-import '@fontsource/m-plus-code-latin/600.css';
-import '@fontsource/m-plus-code-latin/700.css';
 import metatags from 'assets/json/metatags.json';
 import StoreProvider from 'hooks/store/provider';
 import Loader from 'layouts/Loader';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,7 @@
 const tailwindCfg = {
   content: ['./src/**/*.{js,jsx,ts,tsx,css}'],
-  darkMode: 'media',
   theme: {
     extend: {},
-    fontFamily: {
-      sans: ['M PLUS Code Latin', 'sans-serif'],
-    },
-    fontWeight: {
-      lighter: 100,
-      light: 200,
-      semilight: 300,
-      regular: 400,
-      semibold: 500,
-      bold: 600,
-      bolder: 700,
-    },
   },
   variants: {
     extend: {},
