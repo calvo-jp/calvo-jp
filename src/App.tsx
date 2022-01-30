f917aa5159dccbe449ceb8854904ddc34a8117f9
jp calvo <calvojp92@gmail.com>
2022-01-30 19:36:22 +0800

    404 route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import Landing from './pages/Landing';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route path="/" element={<Navigate to="/about" replace />} />
+        <Route path="about" element={<Landing />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
