f5e89c51201a8096e42b4d2a54bac1e0a47bcc5a
jp calvo <calvojp92@gmail.com>
2022-02-02 13:10:47 +0800

    fresh start

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,5 @@
-import * as React from 'react';
-import {
-  BrowserRouter as Router,
-  Navigate,
-  Route,
-  Routes,
-} from 'react-router-dom';
-import spinner from './assets/spinner.svg';
-
-const NotFound = React.lazy(() => import('./pages/NotFound'));
-const Landing = React.lazy(() => import('./pages/Landing'));
-
 const App = () => {
-  return (
-    <React.Suspense fallback={<Loader />}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate to="/about" replace />} />
-          <Route path="about" element={<Landing />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </React.Suspense>
-  );
-};
-
-const Loader = () => {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <img src={spinner} alt="" />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
