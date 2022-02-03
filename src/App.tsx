8b4d78b42e42e49424bac19fff81688e59d6ead0
jp calvo <calvojp92@gmail.com>
2022-02-03 08:57:42 +0800

    init landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import Landing from './pages/Landing';
+
 const App = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/" replace />} />
+        <Route path="/about" element={<Landing />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
