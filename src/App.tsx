9dfec56f3bb2edc63868d4248cb937fe81632557
jp calvo <calvojp92@gmail.com>
2022-02-03 12:27:45 +0800

    router testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import About from './pages/About';
+
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/about" replace />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
