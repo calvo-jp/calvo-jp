f2c657a7d4d7ed6ea9c4eddd8f8f03f611e9af12
jp calvo <calvojp92@gmail.com>
2022-02-06 08:56:51 +0800

    changed global css filename

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles/preflight.css';
+import './main.css';
 
 ReactDOM.render(
   <React.StrictMode>
