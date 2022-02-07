13b404f280881cc0338b2c5896d212a53aeba755
jp calvo <calvojp92@gmail.com>
2022-02-07 12:50:15 +0800

    lazy load

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
+import * as React from 'react';
 import {
   BrowserRouter as Router,
   Navigate,
   Route,
   Routes,
 } from 'react-router-dom';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import NotFound from './pages/NotFound';
-import Projects from './pages/Projects';
+
+const About = React.lazy(() => import('./pages/About'));
+const Contact = React.lazy(() => import('./pages/Contact'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
+const Projects = React.lazy(() => import('./pages/Projects'));
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/about" replace />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <React.Suspense fallback={<Loader />}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/about" replace />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </React.Suspense>
   );
+};
+
+// TODO: use loader svg
+const Loader = () => {
+  return <div>Loading...</div>;
 };
 
 export default App;
