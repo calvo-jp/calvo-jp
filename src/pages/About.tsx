9d6e6386e01b25f94cab02f4462375c7fc3d99b7
jp calvo <calvojp92@gmail.com>
2022-02-07 23:41:12 +0800

    background and footer added here

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,19 @@
+import * as React from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../assets/styles/about.module.scss';
+import FooterSkeleton from '../layouts/FooterSkeleton';
+import ArrowRightIcon from '../widgets/icons/ArrowRight';
+
+const Footer = React.lazy(() => import('../layouts/Footer'));
+const Background = React.lazy(() => import('../layouts/Background'));
 
 const About = () => {
   return (
     <div className={styles.container}>
+      <React.Suspense fallback={<React.Fragment />}>
+        <Background />
+      </React.Suspense>
+
       <section className={styles.about}>
         <h2 className={styles.heading}>
           I'm <span className={styles.gradientText}>JP Calvo</span>.
@@ -15,30 +25,16 @@ const About = () => {
 
         <div className={styles.buttonContainer}>
           <Link to="/projects" className={styles.gradientButton}>
-            Go to Projects
-            <ArrowRightIcon />
+            <span>Go to Projects</span>
+            <ArrowRightIcon width={16} height={16} />
           </Link>
         </div>
       </section>
-    </div>
-  );
-};
 
-const ArrowRightIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-      width={16}
-      height={16}
-    >
-      <path
-        fillRule="evenodd"
-        d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-        clipRule="evenodd"
-      />
-    </svg>
+      <React.Suspense fallback={<FooterSkeleton />}>
+        <Footer />
+      </React.Suspense>
+    </div>
   );
 };
 
