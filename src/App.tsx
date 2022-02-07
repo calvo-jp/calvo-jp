0fb78e5374ff380a902e1156d81a5357e6cb5e86
jp calvo <calvojp92@gmail.com>
2022-02-07 11:46:04 +0800

    projects page init

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
+import Projects from './pages/Projects';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Navigate to="/about" replace />} />
         <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
