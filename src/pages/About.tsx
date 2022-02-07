9ce4f9587ad7ef0f90ce1cb8e1c4ae63b0e9d918
jp calvo <calvojp92@gmail.com>
2022-02-07 11:44:51 +0800

    updates on about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,28 @@
+import { Link } from 'react-router-dom';
+import styles from '../assets/styles/about.module.scss';
 import Layout from '../layouts/Layout';
 
 const About = () => {
-  return <Layout></Layout>;
+  return (
+    <Layout className={styles.container}>
+      <section className={styles.about}>
+        <h2 className={styles.heading}>
+          I'm <span className={styles.gradientText}>JP Calvo</span>.
+        </h2>
+
+        <p className={styles.description}>
+          An aspiring web developer who loves using new tech stacks to develop
+          fast websites with modern design, maintainable and easy to use.
+        </p>
+
+        <div className={styles.buttonContainer}>
+          <Link to="/projects" className={styles.gradientButton}>
+            Go to Projects
+          </Link>
+        </div>
+      </section>
+    </Layout>
+  );
 };
 
 export default About;
