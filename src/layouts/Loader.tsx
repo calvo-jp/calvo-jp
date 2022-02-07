51c2764e82ff0493c5857224994748e765163ab8
jp calvo <calvojp92@gmail.com>
2022-02-07 13:10:43 +0800

    loader update. now using svg

--- src/layouts/Loader.tsx
+++ src/layouts/Loader.tsx
@@ -1,12 +1,10 @@
-// TODO: use loader svg
+import spinner from '../assets/images/spinner.svg';
+import styles from '../assets/styles/loader.module.scss';
+
 const Loader = () => {
   return (
-    <div
-      style={{
-        padding: 8,
-      }}
-    >
-      Loading...
+    <div className={styles.loader}>
+      <img src={spinner} alt="" />
     </div>
   );
 };
