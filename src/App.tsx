d0b9edd42cb09d4f0a4f2b4652d5ad235e8460dc
jp calvo <calvojp92@gmail.com>
2022-02-07 23:38:51 +0800

    removed view project page

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,15 @@ import {
   Routes,
 } from 'react-router-dom';
 import Layout from './layouts/Layout';
-import Project from './pages/project';
 
 const About = React.lazy(() => import('./pages/About'));
 const Contact = React.lazy(() => import('./pages/Contact'));
 const NotFound = React.lazy(() => import('./pages/NotFound'));
 const Projects = React.lazy(() => import('./pages/Projects'));
-const Background = React.lazy(() => import('./layouts/Background'));
 
 const App = () => {
   return (
     <Router>
-      <React.Suspense fallback={<React.Fragment />}>
-        <Background />
-      </React.Suspense>
-
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Navigate to="/about" replace />} />
@@ -29,8 +23,6 @@ const App = () => {
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
         </Route>
-
-        <Route path="/projects/:slug" element={<Project />} />
       </Routes>
     </Router>
   );
