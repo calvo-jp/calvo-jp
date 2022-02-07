6d96f9b2dbe1e000e2317ed6086b27c9fd70450d
jp calvo <calvojp92@gmail.com>
2022-02-07 12:45:07 +0800

    added footer skeleton

---
+++ src/layouts/FooterSkeleton.tsx
@@ -0,0 +1,21 @@
+import styles from '../assets/styles/footer-skeleton.module.scss';
+
+const FooterSkeleton = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.copywright}></div>
+
+      <ul className={styles.icons}>
+        {new Array(5).fill(null).map((_, index) => (
+          <li key={index}>
+            <div className={styles.icon}></div>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
+};
+
+const currentYear = new Date().getFullYear();
+
+export default FooterSkeleton;
