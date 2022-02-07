4c83503981f8b13f937cf4fcc38a03bb2f5ebe23
jp calvo <calvojp92@gmail.com>
2022-02-07 23:41:41 +0800

    refactor

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import styles from '../assets/styles/notfound.module.scss';
+import ArrowLeftIcon from '../widgets/icons/ArrowLeft';
 
 interface NotFoundProps {
   redirect?: string;
@@ -17,28 +18,10 @@ const NotFound = (props: NotFoundProps) => {
       </article>
 
       <Link to={redirect} className={styles.backButton}>
-        <ArrowLeftIcon />
+        <ArrowLeftIcon width={16} height={16} />
         <span>{message}</span>
       </Link>
     </div>
-  );
-};
-
-const ArrowLeftIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-      width={16}
-      height={16}
-    >
-      <path
-        fillRule="evenodd"
-        d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
-        clipRule="evenodd"
-      />
-    </svg>
   );
 };
 
