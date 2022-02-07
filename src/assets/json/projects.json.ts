cc3d185d887ede4bbe4c064c3cdd4c409faf4557
jp calvo <calvojp92@gmail.com>
2022-02-07 18:38:19 +0800

    added list projects

---
+++ src/assets/json/projects.json.ts
@@ -0,0 +1,20 @@
+import IProject from '../../types/project';
+
+const projects: IProject[] = [
+  {
+    slug: 'recipes',
+    name: 'Recipes',
+    description: 'Search or find amazing recipes',
+    screenshots: [],
+    repository: 'https://github.com/calvo-jp/recipes',
+  },
+  {
+    slug: 'hermes',
+    name: 'Hermes',
+    description: 'A light-weight fb messenger alternative',
+    screenshots: [],
+    repository: 'https://github.com/calvo-jp/hermes',
+  },
+];
+
+export default projects;
