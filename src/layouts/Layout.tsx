1ddfa708e836a20b0d34eba4e920edbe6f0a6509
jp calvo <calvojp92@gmail.com>
2022-02-07 10:28:38 +0800

    now allows to pass props to main

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,13 +1,20 @@
+import clsx from 'clsx';
 import * as React from 'react';
 import styles from '../assets/styles/layout.module.scss';
 import Footer from './Footer';
 import Header from './Header';
 
-const Layout: React.FC = ({ children }) => {
+type LayoutProps = React.ComponentProps<'main'>;
+
+const Layout: React.FC<LayoutProps> = ({ children, className, ...props }) => {
   return (
     <div className={styles.container}>
       <Header />
-      <main className={styles.main}>{children}</main>
+
+      <main className={clsx(styles.main, className)} {...props}>
+        {children}
+      </main>
+
       <Footer />
     </div>
   );
