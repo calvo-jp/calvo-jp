55a58b12c04019befdd3359e3c8b6891bd92a4dd
jp calvo <calvojp92@gmail.com>
2022-02-08 15:28:06 +0800

    brb

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,25 @@
+import { Link } from 'react-router-dom';
 import styles from '../assets/styles/contact.module.scss';
+import useSocials from '../hooks/useSocials';
 
 const Contact = () => {
-  return <div className={styles.contact}></div>;
+  const socials = useSocials();
+
+  return (
+    <div className={styles.contact}>
+      <section className={styles.content}>
+        <div className={styles.form}></div>
+
+        <div className={styles.socials}>
+          {socials.map(([SocialIcon, url]) => (
+            <Link key={url} to={url} className={styles.socialIcon}>
+              <SocialIcon />
+            </Link>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Contact;
