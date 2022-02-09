55507fcfdca9aa2bea2a2c6c469d0fd33f3bf842
jp calvo <calvojp92@gmail.com>
2022-02-09 10:19:02 +0800

    temporary ui

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -8,14 +8,25 @@ const Contact = () => {
   return (
     <div className={styles.contact}>
       <section className={styles.content}>
-        <div className={styles.form}></div>
+        <div className={styles.form}>
+          <div className={styles.caption}>
+            <h2>Get in touch</h2>
+          </div>
 
-        <div className={styles.socials}>
-          {socials.map(([SocialIcon, url]) => (
-            <Link key={url} to={url} className={styles.socialIcon}>
-              <SocialIcon />
-            </Link>
-          ))}
+          <input type="text" placeholder="email" autoFocus />
+          <input type="text" placeholder="subject" />
+          <input type="text" placeholder="message" />
+          <button>Send</button>
+        </div>
+
+        <div className={styles.footer}>
+          <div className={styles.socials}>
+            {socials.map(([SocialIcon, url]) => (
+              <Link key={url} to={url} className={styles.socialIcon}>
+                <SocialIcon />
+              </Link>
+            ))}
+          </div>
         </div>
       </section>
     </div>
