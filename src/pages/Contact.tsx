ded200a9540502ea78ef2c29e795300e007553e7
jp calvo <calvojp92@gmail.com>
2022-02-09 16:56:59 +0800

    removed everything for now

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,34 +1,28 @@
+import * as React from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../assets/styles/contact.module.scss';
 import useSocials from '../hooks/useSocials';
 
 const Contact = () => {
+  return (
+    <div className={styles.contact}>
+      <section className={styles.wrapper}>
+        <div></div>
+      </section>
+    </div>
+  );
+};
+
+const Socials = () => {
   const socials = useSocials();
 
   return (
-    <div className={styles.contact}>
-      <section className={styles.content}>
-        <div className={styles.form}>
-          <div className={styles.caption}>
-            <h2>Get in touch</h2>
-          </div>
-
-          <input type="text" placeholder="email" autoFocus />
-          <input type="text" placeholder="subject" />
-          <input type="text" placeholder="message" />
-          <button>Send</button>
-        </div>
-
-        <div className={styles.footer}>
-          <div className={styles.socials}>
-            {socials.map(([SocialIcon, url]) => (
-              <Link key={url} to={url} className={styles.socialIcon}>
-                <SocialIcon />
-              </Link>
-            ))}
-          </div>
-        </div>
-      </section>
+    <div className={styles.socials}>
+      {socials.map(([SocialIcon, url]) => (
+        <Link key={url} to={url} className={styles.socialIcon}>
+          <SocialIcon />
+        </Link>
+      ))}
     </div>
   );
 };
