bd14b69383177ca0907310a7da5d9bc92f088640
jp calvo <calvojp92@gmail.com>
2022-02-18 16:12:51 +0800

    updates on header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -20,10 +20,7 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <Link to="/about" className={styles.logo}>
-        <span>JOHN</span>
-        <span>STUDIOS</span>
-
-        <div className={styles.preview}>PREVIEW</div>
+        <CodeIcon />
       </Link>
 
       <nav
@@ -62,6 +59,22 @@ const Header = () => {
         <div></div>
       </button>
     </header>
+  );
+};
+
+const CodeIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z"
+        clipRule="evenodd"
+      />
+    </svg>
   );
 };
 
