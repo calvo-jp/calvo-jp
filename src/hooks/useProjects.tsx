6a3e8edfa77bb88fb3d81c47841b965c55c17ff2
jp calvo <calvojp92@gmail.com>
2022-03-31 13:44:16 +0800

    updated projects and linked to github

--- src/hooks/useProjects.tsx
+++ src/hooks/useProjects.tsx
@@ -1,27 +1,40 @@
-import hermes from '../assets/images/screenshots/hermes.png';
-import recipes from '../assets/images/screenshots/recipes.png';
-import IProject from '../types/project';
+import IProject from "../types/project";
 
 const items: IProject[] = [
   {
-    slug: 'recipes',
-    name: 'Recipes',
-    description: 'Search or find amazing recipes',
-    image: recipes,
-    repository: 'https://github.com/calvo-jp/recipes',
+    slug: "money-tor",
+    name: "Money-tor",
+    description: "Expense tracker app",
+    image:
+      "https://raw.githubusercontent.com/calvo-jp/react-collections/main/expense-tracker/docs/screenshot.png",
+    repository:
+      "https://github.com/calvo-jp/react-collections/tree/main/expense-tracker",
   },
   {
-    slug: 'hermes',
-    name: 'Hermes',
-    description: 'A light-weight fb messenger alternative',
-    image: hermes,
-    repository: 'https://github.com/calvo-jp/hermes',
+    slug: "todo",
+    name: "Todo app",
+    description: "A typical todo app",
+    image:
+      "https://raw.githubusercontent.com/calvo-jp/react-collections/main/todo/docs/screenshot-1.png",
+    repository: "https://github.com/calvo-jp/react-collections/tree/main/todo",
   },
   {
-    slug: 'pets',
-    name: 'Pets',
-    description: 'Buy or sell a pet',
-    repository: 'https://github.com/calvo-jp/pets',
+    slug: "pokemon",
+    name: "Pokemon",
+    description: "Get info about your favorite pokemon",
+    image:
+      "https://raw.githubusercontent.com/calvo-jp/react-collections/main/pokemon/docs/screenshot.png",
+    repository:
+      "https://github.com/calvo-jp/react-collections/tree/main/pokemon",
+  },
+  {
+    slug: "amazing-recipes",
+    name: "Recipes",
+    description: "Search or find amazing recipes",
+    image:
+      "https://raw.githubusercontent.com/calvo-jp/react-collections/main/amazing-recipes/docs/screenshot.png",
+    repository:
+      "https://github.com/calvo-jp/react-collections/tree/main/amazing-recipes",
   },
 ];
 
