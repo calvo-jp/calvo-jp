6305544cc038cc4466c2c30be8c18158abdfec1e
jp calvo <calvojp92@gmail.com>
2022-03-31 13:54:27 +0800

    dropped FC use. will no longer use from now on

--- src/layouts/Container.tsx
+++ src/layouts/Container.tsx
@@ -1,12 +1,12 @@
 import clsx from "clsx";
-import * as React from "react";
+import { ComponentProps, PropsWithChildren } from "react";
 import styles from "../assets/styles/layout.module.scss";
 
-const Container: React.FC<React.ComponentProps<"div">> = ({
+const Container = ({
   children,
   className,
   ...props
-}) => {
+}: PropsWithChildren<ComponentProps<"div">>) => {
   return (
     <div className={clsx(styles.innerWrapper, className)} {...props}>
       {children}
