6e29049c5342199d245e5cade82ca449b3e70341
jp calvo <calvojp92@gmail.com>
2022-03-31 13:48:21 +0800

    refactor

--- src/hooks/useSocials.tsx
+++ src/hooks/useSocials.tsx
@@ -1,17 +1,17 @@
-import FacebookIcon from '../widgets/icons/Facebook';
-import GithubIcon from '../widgets/icons/Github';
-import MessengerIcon from '../widgets/icons/Messenger';
-import TwitterIcon from '../widgets/icons/Twitter';
+import FacebookIcon from "../widgets/icons/Facebook";
+import GithubIcon from "../widgets/icons/Github";
+import MessengerIcon from "../widgets/icons/Messenger";
+import TwitterIcon from "../widgets/icons/Twitter";
 
 const useSocials = (): [
-  icon: React.FC<React.ComponentProps<'svg'>>,
+  icon: React.FC<React.ComponentProps<"svg">>,
   url: string
 ][] => {
   return [
-    [FacebookIcon, 'https://facebook.com/calvojp'],
-    [MessengerIcon, 'https://m.me/calvojp'],
-    [TwitterIcon, 'https://twitter.com/calvo__jp'],
-    [GithubIcon, 'https://github.com/calvo-jp'],
+    [FacebookIcon, "https://facebook.com/calvojp"],
+    [MessengerIcon, "https://m.me/calvojp"],
+    [TwitterIcon, "https://twitter.com/calvo__jp"],
+    [GithubIcon, "https://github.com/calvo-jp"],
   ];
 };
 
